fad9a70332949eb852960ee76cb86bd05a511507
gt19tg <gelutila8@gmail.com>
2024-08-07 18:00:48 +0300

    Python programming codes!!!

---
+++ py.py
@@ -0,0 +1,16 @@
+# Exe 1
+gtst = 'Day 7' 
+# Exe 2
+print(f'Hello Today is our {gtst} course') 
+# Exe 3
+Even_Numbers = ('0','2','4','6','8','12')
+print('The 1st Even number is:' , Even_Numbers[4])
+print('The 2nd Even number is:' , Even_Numbers[0])
+print('The 3rd Even number is:' , Even_Numbers[1])
+# Exe 4
+Fruits = {'Appel':'10 ' , 'Banana':'15' , 'Pineapple':'20'}
+# Exe 5
+Choice = { "Apple" : "10 birr" , "Banana" : "15 birr" , "Pineapple":"20 birr" }
+print (f'The value of Apple is : {Choice['Apple']} birr')
+print (f'The value of Banana is : {Choice['Banana']} birr')
+print (f'The value of Pineapple is : {Choice['Pineapple']} birr')
